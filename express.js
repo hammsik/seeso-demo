e41b2a74acd1eab8cfa46f0b4d91fed4a99b8265
hammsik <asbbrp@gmail.com>
2024-01-09 10:40:42 +0900

    Fix: test1

--- express.js
+++ express.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const app = express();
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
+const app = express();
 const port = 8083;
+
+const options = {
+    key: fs.readFileSync('./keys/private.pem'),
+    cert: fs.readFileSync('./keys/public.pem')
+  };
 
 app.use((req, res, next) => {
     res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
@@ -9,13 +16,12 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use( '/', express.static( path.join(__dirname, 'dist') ));  
-// 이 부분이 없으면 아래코드에서 index.html을 로드하지 못한다.
+app.use( '/', express.static( path.join(__dirname, 'dist') )); // 아래코드에서 index.html을 로드하기 위한 path 설정
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'dist/index.html'));  
-})
-// 기본 경로 '/'을 통해 빌드된 dist/index.html 파일을 로드시킨다.
+}) // 기본 경로 '/'을 통해 빌드된 dist/index.html 파일을 로드
 
-app.listen(port, () => {
+const server = https.createServer(options, app);
+server.listen(port, () => {
     console.log(`Express.js 서버가 http://localhost:${port} 포트에서 실행 중입니다.`);
 });
