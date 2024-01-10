5984f010ccd589df9bdf6c0510672bc1e214b7e6
hammsik <asbbrp@gmail.com>
2024-01-10 14:30:03 +0900

    Fix: 주석 및 코드 포맷 수정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import EasySeeSo from 'seeso/easy-seeso';
-// import { TrackingState } from 'seeso'
+import { TrackingState } from 'seeso'
 
 const licenseKey = 'dev_r6lhgvzp6c9qrujmix67580y3r207itavw0fonmf';
 
@@ -13,6 +13,8 @@ export default {
             windowCenter_y: 0,
         };
     },
+
+
     methods: {
         afterInitialized() {
             console.log('sdk init success!')
@@ -22,17 +24,17 @@ export default {
             this.text = '실패'
         },
         onGaze(gazeInfo) {
-            // if (gazeInfo.trackingState === TrackingState.SUCCESS) {
-            let canvas = document.getElementById("output")
-            canvas.width = window.innerWidth
-            canvas.height = window.innerHeight
-            let ctx = canvas.getContext("2d");
-            ctx.fillStyle = '#FF0000'
-            ctx.clearRect(0, 0, canvas.width, canvas.height)
-            ctx.beginPath();
-            ctx.arc(gazeInfo.x, gazeInfo.y, 10, 0, Math.PI * 2, true);
-            ctx.fill();
-            // }
+            if (gazeInfo.trackingState === TrackingState.SUCCESS) {
+                let canvas = document.getElementById("output")
+                // canvas.width = window.innerWidth
+                // canvas.height = window.innerHeight
+                let ctx = canvas.getContext("2d");
+                ctx.fillStyle = '#FF0000'
+                ctx.clearRect(0, 0, canvas.width, canvas.height)
+                ctx.beginPath();
+                ctx.arc(gazeInfo.x, gazeInfo.y, 10, 0, Math.PI * 2, true);
+                ctx.fill();
+            }
         },
         parseCalibrationDataInQueryString() {
             const href = window.location.href
@@ -72,7 +74,7 @@ export default {
 
             })
             spanList.forEach((span) => {
-                console.log(parseInt(span.id) + ', ' + minSpanId);
+                // console.log(parseInt(span.id) + ', ' + minSpanId);
                 span.classList.add('highlight');
             });
         },
@@ -80,8 +82,8 @@ export default {
             var minGap = window.innerHeight;
             var nearElementList = [];
             spanElements.forEach((span) => {
-                var gap = Math.abs(span.getBoundingClientRect().y - this.windowCenter_y);
-                console.log(gap);
+                var gap = Math.abs(span.getBoundingClientRect().y - this.windowCenter_y); // 일단은 화면 가운데로
+                // console.log(gap);
                 if (gap < minGap) {
                     nearElementList = [];
                     minGap = gap;
@@ -90,15 +92,18 @@ export default {
                     nearElementList.push(span);
                 }
             })
-            console.log(nearElementList);
             if (!(nearElementList[0].hasAttribute('id'))) nearElementList.shift();
             return nearElementList;
         }
     },
+
+
     created() {
         this.windowCenter_x = window.innerWidth / 2;
         this.windowCenter_y = window.innerHeight / 2;
     },
+
+
     async mounted() {
         await this.makingText();
         const spanElements = document.querySelectorAll('span');
@@ -112,22 +117,29 @@ export default {
             if (!ticking) {
                 window.requestAnimationFrame(() => {
                     this.highlightAction(spanElements);
-                    ticking = false;
+                    ticking = false; ``
                 });
 
                 ticking = true;
             }
         });
+        this.$nextTick(() => {
+            const canvas = document.getElementById("output");
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        });
         // (async () => { this.seeso.init(licenseKey, this.afterInitialized, this.afterFailed) })()
-        // (async () => { this.seeso.init(licenseKey, this.onGaze, this.afterFailed) })()
 
         // const calibrationData = this.parseCalibrationDataInQueryString()
         // if (calibrationData) {
-        //     (async () => {
-        //         this.seeso = new EasySeeSo();
-        //         // Don't forget to enter your license key.`
-        //         await this.seeso.init(licenseKey, async () => { this.seeso.startTracking(this.onGaze) }, this.afterFailed)
-        //     })()
+        (async () => {
+            this.seeso = new EasySeeSo();
+            // Don't forget to enter your license key.`
+            await this.seeso.init(licenseKey, async () => {
+                await this.seeso.startTracking(this.onGaze);
+                // await this.seeSo.setCalibrationData(calibrationData);
+            }, this.afterFailed)
+        })()
         // } else {
         //     console.log('No calibration data given.')
         //     const calibrationButton = document.getElementById('calibrationButton')
@@ -153,42 +165,27 @@ span {
     }
 }
 
-@keyframes fadeOut {
-    from {
-        background-color: rgb(204, 216, 255);
-    }
-
-    to {
-        background-color: transparent
-    }
-}
-
 .highlight {
     background-color: rgb(204, 216, 255);
-    /* transition: background-color 1s ease; */
     animation-name: fadeIn;
     animation-duration: 0.5s;
-}
-
-.removeHighlight {
-    background-color: transparent;
-    animation-name: fadeOut;
-    animation-duration: 0.8s;
 }
 </style>
 
 <template>
-    <!-- <canvas id="output"></canvas> -->
-    <!-- <button id="calibrationButton" style="margin: 0 auto;"> Start Calibration </button> -->
-    <h2 id="title">{{ text }}</h2>
-    <h2 id="gazeInfo" style="text-align: center; color: red;"></h2>
-    <span v-for='(item, index) in textArr' :key="index">
-        &nbsp;
-        <span v-for='(item2, index2) in item' :key="index2"
-            :id="(textArr.slice(0, index).reduce((acc, val) => acc + val.length, 0) + index2)">
-            {{ item2 + ' ' }}
-        </span>
-        <br>
-    </span>
+    <div style="position: relative">
+        <canvas id="output" style="overflow: hidden; position: absolute; z-index: 1;"></canvas>
+        <!-- <button id="calibrationButton" style="margin: 0 auto;"> Start Calibration </button> -->
+        <div id="text-layout" style="position: absolute; z-index: 2;">
+            <span v-for='(item, index) in textArr' :key="index">
+                &nbsp;
+                <span v-for='(item2, index2) in item' :key="index2"
+                    :id="(textArr.slice(0, index).reduce((acc, val) => acc + val.length, 0) + index2)">
+                    {{ item2 + ' ' }}
+                </span>
+                <br>
+            </span>
+        </div>
+    </div>
 </template>
   
